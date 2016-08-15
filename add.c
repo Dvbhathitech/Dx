437d240f91a9bc1a5bff236e977b19fd9b97c257
Deexit <dvbhat@gmail.com>
2016-08-15 20:57:28 +0530

    changes

---
+++ add.c
@@ -0,0 +1,22 @@
+nclude<stdio.h>
+#include<conio.h>
+
+void main(){
+
+int one, two, add;
+
+printf("Enter first number - ");
+
+scanf("%d",&one);
+
+printf("Enter second number - ");
+
+scanf("%d",&two);
+
+sub = one * two;
+
+printf("The addition of numbers %d and %d is %d",one,two,sub);
+
+getch();
+
+}
