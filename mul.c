66065551857a07b9ae935b7aafc87043e76a82ba
Deexit <dvbhat@gmail.com>
2016-08-15 20:46:22 +0530

    commit is done

---
+++ mul.c
@@ -0,0 +1,21 @@
+nclude<stdio.h>
+#include<conio.h>
+
+void main(){
+
+int one, two, mul;
+
+printf("Enter first number - ");
+
+scanf("%d",&one);
+
+printf("Enter second number - ");
+
+scanf("%d",&two);
+
+mul = one * two;
+
+printf("The multiplication of numbers %d and %d is %d",one,two,mul);
+getch();
+
+}
